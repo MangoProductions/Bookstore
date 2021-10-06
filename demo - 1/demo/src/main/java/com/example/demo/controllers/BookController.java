a6aa4fa0c7ff48a43b724afb67f876cc0a4db492
MangoProductions <62751590+MangoProductions@users.noreply.github.com>
2021-10-06 20:56:45 +0300

    Switch to non-working working method

--- demo - 1/demo/src/main/java/com/example/demo/controllers/BookController.java
+++ demo - 1/demo/src/main/java/com/example/demo/controllers/BookController.java
@@ -1,31 +1,41 @@
 package com.example.demo.controllers;
-import java.awt.print.Book;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import com.example.demo.content.Books;
 
-
-
+import content.Book;
+import content.BookRepository;
 
 @Controller
 public class BookController {
+	@Autowired
+	private BookRepository repository; 
+	
+    @RequestMapping(value= {"/", "/booklist"})
+    public String studentList(Model model) {	
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
+  
+    @RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    }     
+    
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }    
 
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
-  public String indexModel(Model model) {
-    model.addAttribute("book", new Books());
-    return "index";
-  }
-
-  @PostMapping("/hello")
-  public String showAll(Model model) {
-    model.addAttribute("book", Books.findAll());
-    return "result";
-  }
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }     
 }
