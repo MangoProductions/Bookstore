34e54011131f06c573e6c8cad9970b7ba357613b
MangoProductions <62751590+MangoProductions@users.noreply.github.com>
2021-10-07 00:44:00 +0300

    Minor adjustments

--- demo - 1/demo/src/main/java/com/example/demo/controllers/BookController.java
+++ demo - 1/demo/src/main/java/com/example/demo/controllers/BookController.java
@@ -1,14 +1,16 @@
 package com.example.demo.controllers;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import content.Book;
-import content.BookRepository;
+import uncategorizedbackend.Book;
+import uncategorizedbackend.BookRepository;
 
 @Controller
 public class BookController {
@@ -16,7 +18,7 @@ public class BookController {
 	private BookRepository repository; 
 	
     @RequestMapping(value= {"/", "/booklist"})
-    public String studentList(Model model) {	
+    public String bookList(Model model) {	
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
@@ -34,7 +36,7 @@ public class BookController {
     }    
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+    public String deleteBook(@PathVariable("id") long bookId, Model model) {
     	repository.deleteById(bookId);
         return "redirect:../booklist";
     }     
